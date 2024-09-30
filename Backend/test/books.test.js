d6633a1440d5e1ddac9c4cfe21aeb2296ba4c558
Jes360 <s222598312@deakin.edu.au>
2024-09-30 23:13:27 +1000

    Update books.test.js

--- Backend/test/books.test.js
+++ Backend/test/books.test.js
@@ -1,21 +1,6 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongoose = require('mongoose');
-
-let mongoServer;
-
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const mongoUri = mongoServer.getUri();
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
-});
 // books.test.js
 describe('Sample Test', () => {
-    test('true should be true', () => {
-      expect(true).toBe(true);
-    });
+  test('true should be true', () => {
+    expect(true).toBe(true);
   });
-  
+});
